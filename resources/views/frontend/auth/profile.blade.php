a359f796cbaddd7cac5220c7c555367c68470eb1
Rojifa-Akhter <mstrojifaakhter@gmail.com>
2026-08-30 11:26:00 +0600

    image

--- resources/views/frontend/auth/profile.blade.php
+++ resources/views/frontend/auth/profile.blade.php
@@ -105,16 +105,12 @@
                             <i class="fa-solid fa-users-rectangle w-5"></i>
                             <span class="text-base font-medium">My Leads</span>
                         </button>
-                        {{-- <button @click="activeTab = 'team'"
+                    <button @click="activeTab = 'team'"
                             :class="activeTab === 'team' ? 'bg-[#003B7A] text-white' : 'text-gray-700 hover:bg-gray-50'"
                             class="flex items-center gap-3 px-4 py-3 transition-all border-b border-gray-50 text-left">
                             <i class="fa-solid fa-arrows-down-to-people w-5"></i>
                             <span class="text-base font-medium">Team</span>
-                        </button> --}}
-                        <a href="{{ route('profile.team') }}"
-                            class="flex items-center gap-3 px-4 py-3 border-b {{ request()->routeIs('profile.team') ? 'bg-[#003B7A] text-white' : 'text-gray-700' }}">
-                            <i class="fa-solid fa-arrows-down-to-people w-5"></i> <span>Team</span>
-                        </a>
+                        </button>
                         <!-- My Coupons Tab Button -->
                         <button @click="activeTab = 'coupons'"
                             :class="activeTab === 'coupons' ? 'bg-[#003B7A] text-white' : 'text-gray-700 hover:bg-gray-50'"
