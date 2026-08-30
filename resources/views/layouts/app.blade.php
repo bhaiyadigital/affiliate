5088bab5c4f9e251a163271de09dcbb02d80baa2
kalam714 <56288400+kalam714@users.noreply.github.com>
2026-08-30 16:10:55 +0600

    Add favicon link to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="robots" content="noindex, nofollow">
+        <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
     <title>{{ $title ?? 'Bhaiya Affiliate Program' }} </title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- Scripts -->
